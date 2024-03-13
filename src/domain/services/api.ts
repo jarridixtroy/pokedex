f358f8dee569a63ac552de03aeda979bf62cc233
jarridixtroy <xabi.arriaga@gmail.com>
2024-03-13 12:08:15 +0100

    fix: arreglado el cambio de json a PokemonDTO

--- src/domain/services/api.ts
+++ src/domain/services/api.ts
@@ -1,14 +1,26 @@
+import PokemonDTO from "../dto/pokemonDTO";
+
 const BASE_URL = "https://pokeapi.co/api/v2";
 
 export const fetchPokemonList = async (): Promise<JSON[]> => {
   const response = await fetch(`${BASE_URL}/pokemon/?limit=151&offset=1`);
   const data = await response.json();
-  const pokemon_list: JSON[] = data.results;
+  const pokemon_list = data.results;
   return pokemon_list;
 };
 
-export const fetchPokemonDetails = async (id: number): Promise<JSON> => {
+export const fetchPokemonDetails = async (id: number): Promise<PokemonDTO> => {
   const response = await fetch(`${BASE_URL}/pokemon/${id}`);
-  const data: JSON = await response.json();
-  return data;
+  const data = await response.json();
+
+  const details: PokemonDTO = {
+    height: data.height,
+    weight: data.weight,
+    id: data.id,
+    name: data.name,
+    sprites: data.sprites,
+    types: data.types,
+  };
+
+  return details;
 };
