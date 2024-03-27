e61abc4ce62930e9f4449cd04a7cf0d4a0d5fef4
jarridixtroy <xabi.arriaga@gmail.com>
2024-03-27 10:03:32 +0100

    feat: añadidios styled components

--- src/componentes/PokemonCard.tsx
+++ src/componentes/PokemonCard.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { Pokemon } from "../domain/models/pokemon.model.ts";
 import Tag from "./Tags.tsx";
 
@@ -9,69 +10,125 @@ const PokemonCard: React.FC<Props> = ({ pokemon }) => {
   const { name, id, types, height, weight, sprite } = pokemon;
   const typeClass = types[0];
 
-  let ceros = null;
-  if (id < 10) {
-    ceros = "00";
-  } else if (id < 100) {
-    ceros = "0";
-  }
+  const get3DigitsName = () => {
+    if (id < 10) return "#00" + id;
+
+    if (id < 100) return "#0" + id;
+
+    return "#" + id;
+  };
 
   let propperName = name.charAt(0).toUpperCase() + name.slice(1);
 
   return (
     <div className={`pokemon-card ${typeClass}`}>
-      <div className="col">
-        <span text-align="center" className="poppins-bold">
-          <h2>{propperName}</h2>
-          <h4>
-            {" "}
-            #{ceros}
-            {id}
-          </h4>
-        </span>
+      <CardHeader>
+        <Nombre>{propperName}</Nombre>
+        <Id>{get3DigitsName()}</Id>
+      </CardHeader>
 
-        <img src={sprite} alt={name} className="pokemon-image" />
-        <br></br>
-        <div className="details">
-          <br></br>
+      <Imagen src={sprite} alt={propperName} />
+
+      <div className="card__details">
+        <Tipos>
           {types.map((tipo, index) => (
             <Tag key={index} tipo={tipo}></Tag>
           ))}
-
-          <br></br>
-
-          <p className={`poppins-bold ${typeClass}`} color={typeClass}>
-            About
-          </p>
-          <div className="col" text-align="left">
-            <span>
-              <img
-                src="./iconos/altura.png"
-                width="10px"
-                height="16px"
-                alt=""
-              />
-              {height / 10} m <br /> Altura
-            </span>
-          </div>
-          <div className="col" text-align="center">
-            <span className="vl"></span>
-          </div>
-          <div className="col" align-self="right">
-            <span>
+        </Tipos>
+        <About className={`about-${typeClass}`}>About</About>
+        <Size>
+          <PesoAltura>
+            <SeccionPesoAltura>
               <img
                 src="./iconos/pesopokemon.png"
-                width="16px"
-                height="16px"
-                alt=""
+                width="18px"
+                height="20px"
+                alt="peso"
               />
-              {weight / 10} Kg <br /> Peso
-            </span>
-          </div>
-        </div>
+              {weight / 10} Kg
+            </SeccionPesoAltura>
+            <SeccionPesoAltura>Weight</SeccionPesoAltura>
+          </PesoAltura>
+          <section className="vl"></section>
+          <PesoAltura>
+            <SeccionPesoAltura>
+              <img
+                src="./iconos/altura.png"
+                width="8px"
+                height="16px"
+                alt={propperName}
+              />
+              {height / 10} m{" "}
+            </SeccionPesoAltura>
+            <SeccionPesoAltura>Height</SeccionPesoAltura>
+          </PesoAltura>
+        </Size>
+        <span>
+          Pikachu es el mas fiera keloke. Esta en su prime yeah, yeah yeah
+        </span>
       </div>
     </div>
   );
 };
 
+const Nombre = styled.h2`
+  display: flex;
+  align-items: flex-start;
+  font-family: "Poppins-bold", sans-serif;
+  font-weight: 700;
+  font-size: 24px;
+  margin: 0px;
+  height: auto;
+  width: auto;
+`;
+const Id = styled.span`
+  display: flex;
+  align-items: center;
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+  font-size: 17px;
+`;
+
+const Tipos = styled.div`
+  font-family: "Poppins-bold", sans-serif;
+  font-weight: 700;
+  padding: 4px;
+  width: auto;
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+`;
+
+const CardHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-beetween;
+  gap: 100px;
+  align-content: center;
+`;
+const About = styled.section`
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+`;
+const Size = styled.div`
+  display: flex;
+  justify-content: center;
+
+  margin-bottom: 10px;
+  gap: 24px;
+`;
+const PesoAltura = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const SeccionPesoAltura = styled.span`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 3px;
+`;
+const Imagen = styled.img`
+  width: 150px;
+  height: auto;
+`;
 export default PokemonCard;
