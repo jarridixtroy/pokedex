6751b1abf87b7c0070759b82354b73eaedfd24ea
jarridixtroy <xabi.arriaga@gmail.com>
2024-03-29 22:54:04 +0100

    fix: mejorado el aspecto searchBar

--- src/componentes/SearchBar.tsx
+++ src/componentes/SearchBar.tsx
@@ -1,8 +1,7 @@
-
 import React, { ChangeEvent, useState } from "react";
 import { act } from "react-dom/test-utils";
 import styled from "styled-components";
-import busquedaIcono from '../iconos/busquedaIcono.png'; 
+import busquedaIcono from "../iconos/busquedaIcono.png";
 interface SearchBarProps {
   onSearch: (term: string) => void;
 }
@@ -25,7 +24,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
       value={searchTerm}
       onChange={handleSearch}
       className="search-bar"
-    />
+    ></Barra>
   );
 };
 
@@ -33,13 +32,15 @@ export default SearchBar;
 
 const Barra = styled.input`
   width: 40%;
-  padding: 8px;
+  padding-left: 30px;
   margin: 0 auto; /* Centra el elemento horizontalmente */
   display: block;
+  border-width: 2px;
+  height: 20px;
   border-radius: 10px;
   background-image: url(${busquedaIcono});
-  background-size: 20px; 
-  background-position: left 10px center; 
+  background-size: 20px;
+  background-position: left 10px center;
   background-repeat: no-repeat;
-  padding-right: 30px; 
+  padding-right: 30px;
 `;
