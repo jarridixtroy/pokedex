948fb13c0c374a2aedcf0a846470c7aa2e8364aa
jarridixtroy <xabi.arriaga@gmail.com>
2024-04-04 14:01:09 +0200

    version 2 de los servicios

--- src/domain/services/getPokemonDetails.ts
+++ src/domain/services/getPokemonDetails.ts
@@ -1,4 +1,7 @@
 import PokemonDTO from "../dto/pokemonDTO";
+import { Pokemon } from "../models/Pokemon";
+import { getPokemonDescription } from "./getPokemonDescription";
+import { transform } from "../factories/buildPokemon";
 
 const BASE_URL = "https://pokeapi.co/api/v2";
 
@@ -16,4 +19,21 @@ export async function getPokemonDetails(id: number): Promise<PokemonDTO> {
   };
 
   return details;
+}
+
+export async function getPokemonDetailsV2(id: number): Promise<Pokemon> {
+  const response = await fetch(`${BASE_URL}/pokemon/${id}`);
+  const data = await response.json();
+  const response_description = await getPokemonDescription(id);
+  const details: PokemonDTO = {
+    height: data.height,
+    weight: data.weight,
+    id: data.id,
+    name: data.name,
+    sprites: data.sprites,
+    types: data.types,
+  };
+  const pokemonDetails = transform(details);
+
+  return pokemonDetails;
 }
--- src/domain/services/getPokemonList.ts
+++ src/domain/services/getPokemonList.ts
@@ -1,8 +1,19 @@
+import { Pokemon } from "../models/Pokemon";
+import { getPokemonDetails, getPokemonDetailsV2 } from "./getPokemonDetails";
+
 const BASE_URL = "https://pokeapi.co/api/v2";
 
 export async function getPokemonList(): Promise<JSON[]> {
   const response = await fetch(`${BASE_URL}/pokemon/?limit=151&offset=1`);
   const data = await response.json();
   const pokemon_list = data.results;
+  return pokemon_list;
+}
+
+export async function getPokemonListV2(cuantos: number): Promise<Pokemon[]> {
+  const pokemon_list: Pokemon[] = new Array(cuantos);
+  for (let i = 0; i < cuantos; i++) {
+    pokemon_list[i] = await getPokemonDetailsV2(i + 1);
+  }
   return pokemon_list;
 }
