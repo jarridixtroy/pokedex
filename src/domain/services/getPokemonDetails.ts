5e0700d435cc09a9e7b19e52b5a36881a94379e3
jarridixtroy <xabi.arriaga@gmail.com>
2024-04-11 09:29:46 +0200

    eliminada la primera version del servicio

--- src/domain/services/getPokemonDetails.ts
+++ src/domain/services/getPokemonDetails.ts
@@ -3,25 +3,9 @@ import { Pokemon } from "../models/Pokemon";
 import { transform } from "../factories/buildPokemon";
 import { getSpanishDescription } from "./getPokemonDescription";
 
-const BASE_URL = "https://pokeapi.co/api/v2";
 
-export async function getPokemonDetails(id: number): Promise<PokemonDTO> {
-  const response = await fetch(`${BASE_URL}/pokemon/${id}`);
-  const data = await response.json();
 
-  const details: PokemonDTO = {
-    height: data.height,
-    weight: data.weight,
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-    types: data.types,
-  };
-
-  return details;
-}
-
-export async function getPokeDetails(url: string): Promise<Pokemon> {
+export async function getPokemon(url: string): Promise<Pokemon> {
   const response = await fetch(url);
   const data = await response.json();
   const description = await getSpanishDescription(data.id);
